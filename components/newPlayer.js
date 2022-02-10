e8be361ecd4d84b476cb0963e44c356241333cf4
Dawit <dwtmekonnen123@gmail.com>
2022-02-10 14:09:39 +0300

    improved the audio player and removed some bugs

--- components/newPlayer.js
+++ components/newPlayer.js
@@ -1,26 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { IoMdArrowRoundBack } from 'react-icons/io'
-import { AiFillPlayCircle, AiFillPauseCircle, AiOutlineForward } from 'react-icons/ai'
+import { AiFillPlayCircle, AiFillPauseCircle, AiOutlineForward, AiOutlineBackward } from 'react-icons/ai'
 import { RiRewindMiniFill } from 'react-icons/ri'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { data } from '../data'
-import rewind from '../assets/rewind.png'
+import ReactPlayer from 'react-player'
+
 const NewPlayer = () => {
 
     let [audioData, setAudioData] = useState({})
-    const [isPlaying, setIsPlaying] = useState(false);
+    const [played, setPlayed] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(0);
     const [duration, setDuration] = useState(0);
-    const [currentTime, setCurrentTime] = useState(0);
-
-    // references
-    const audioPlayer = useRef();   // reference our audio component
-    const progressBar = useRef();   // reference our progress bar
-    const animationRef = useRef();  // reference the animation
+    const progress = useRef(null);
+    const player = useRef(null);
 
     const router = useRouter()
     let id = parseFloat(router.query.id)
-
     useEffect(() => {
         console.log(data, id);
         data.map(el => {
@@ -29,111 +26,85 @@ const NewPlayer = () => {
             }
         })
 
+
     }, [id])
-
-
-    useEffect(() => {
-        const seconds = Math.floor(audioPlayer.current.duration);
-        setDuration(seconds);
-        progressBar.current.max = seconds;
-    }, [audioPlayer.current?.loadedmetadata, audioPlayer?.current?.readyState, id]);
-
-    const calculateTime = (secs) => {
-        const minutes = Math.floor(secs / 60);
-        const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-        const seconds = Math.floor(secs % 60);
-        const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-        return `${returnedMinutes}:${returnedSeconds}`;
+    const togglePlaying = () => {
+        setIsPlaying(isPlaying => !isPlaying)
+    }
+    const onDuration = (duration) => {
+        setDuration(duration)
+    }
+    const handleChange = () => {
+        // setPlayed(progress.current.value)
+        player.current.seekTo(progress.current.value)
     }
 
-    const togglePlayPause = () => {
-        const prevValue = isPlaying;
-        setIsPlaying(!prevValue);
-        if (!prevValue) {
-            audioPlayer.current.play();
-            animationRef.current = requestAnimationFrame(whilePlaying)
-        } else {
-            audioPlayer.current.pause();
-            cancelAnimationFrame(animationRef.current);
-        }
+
+    const handleBackwards = () => {
+        // setPlayed(played => played + 30)
+        player.current.seekTo(played - 30)
     }
+    const handleForwards = () => {
+        player.current.seekTo(played + 30)
 
-    const whilePlaying = () => {
-        progressBar?.current.value = audioPlayer.current.currentTime;
-        changePlayerCurrentTime();
-        animationRef.current = requestAnimationFrame(whilePlaying);
-    }
 
-    const changeRange = () => {
-        audioPlayer.current.currentTime = progressBar.current.value;
-        changePlayerCurrentTime();
-    }
-
-    const changePlayerCurrentTime = () => {
-        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`)
-        setCurrentTime(progressBar.current.value);
-    }
-
-    const backThirty = () => {
-        progressBar.current.value = Number(progressBar.current.value - 30);
-        console.log(progressBar.current.value);
-
-        changeRange();
-    }
-
-    const forwardThirty = () => {
-        progressBar.current.value = Number(progressBar.current.value + 30);
-        console.log(progressBar.current.value);
-        changeRange();
     }
 
     if (audioData) {
 
-        return (<div className='h-fit bg-indigo-400 py-12 px-32'>
-            <audio ref={audioPlayer} src={audioData.audio} preload="metadata"></audio>
-            <div className='max-w-screen-md m-auto'>
-                <div className='flex items-center text-white text-xl cursor-pointer'>
+        return (
+            <div className='h-fit bg-indigo-400 py-12 px-32'>
+                {/* <audio ref={audioPlayer} src={audioData.audio} preload="metadata"></audio> */}
+                <div className='max-w-screen-md m-auto'>
+                    <div className='flex items-center text-white text-xl cursor-pointer'>
 
-                    <Link href='/podcast'><IoMdArrowRoundBack /></Link>
-                    <span className='mx-2'> <Link href='/podcast'>Back</Link></span>
-                </div>
-
-                <div className='flex items-center justify-center mb-24 gap-24 pt-8'>
-                    <img className='gap-10 max-w-md' src={audioData.image} alt='album cover' />
-                    <div>
-                        <h1 className='text-5xl text-white font-extrabold'>{audioData.title}</h1>
+                        <Link href='/podcast'><IoMdArrowRoundBack /></Link>
+                        <span className='mx-2'> <Link href='/podcast'>Back</Link></span>
                     </div>
-                </div>
-                <div className='flex items-center justify-center '>
-                    <span className="text-white">{(duration && !isNaN(duration)) ? calculateTime(duration) : '--|--'}</span>
-                    <input type='range' ref={progressBar} onChange={changeRange} className='
+
+                    <div className='flex items-center justify-center mb-24 gap-24 pt-8'>
+                        <img className='gap-10 max-w-md' src={audioData.image} alt='album cover' />
+                        <div>
+                            <h1 className='text-5xl text-white font-extrabold'>{audioData.title}</h1>
+                        </div>
+                    </div>
+                    <div className='flex items-center justify-center '>
+                        <span className="text-white">{(played / 60).toFixed(2)}</span>
+                        <input type='range' ref={progress} onChange={handleChange} defaultValue='0' value={played} max={duration} className='
                     mx-4 form-range
                     appearance-none
                     rounded-3xl
                     h-2
                     w-full
-                    h-6
+                   
                     p-0
-                    bg-white-300
+                    bg-gray-300
                     focus:outline-none focus:ring-0 focus:shadow-none' />
-                    <span className="text-white">{calculateTime(currentTime)}</span>
 
-                </div>
-                <div className='flex justify-between items-center text-7xl text-white mt-12'>
-                    <span className='cursor-pointer' onClick={backThirty}><img className='w-11' src={rewind.src} /></span>
-                    <span className='cursor-pointer' onClick={togglePlayPause}>
-                        {isPlaying ? <AiFillPauseCircle /> : <AiFillPlayCircle />}
-                    </span>
-                    <span></span>
-                    {/* <span className='cursor-pointer' onClick={forwardThirty}> <AiOutlineForward /></span> */}
-                </div>
-            </div>
-        </div >);
+                        <span className="text-white">{duration}</span>
+
+                    </div>
+                    <div className='flex justify-between items-center text-7xl text-white mt-12'>
+
+                        <span className='cursor-pointer' onClick={handleBackwards}> <AiOutlineBackward /></span>
+                        <span className='cursor-pointer' onClick={togglePlaying}>
+                            {
+                                isPlaying ? <AiFillPauseCircle /> : <AiFillPlayCircle />
+                            }
+
+                        </span>
+                        <span className='cursor-pointer' onClick={handleForwards} > <AiOutlineForward /></span>
+                    </div>
+                </div >
+                <ReactPlayer url={`${audioData.audio}`} width="100%"
+                    onProgress={(progress) => {
+                        setPlayed(progress.playedSeconds);
+                    }}
+                    ref={player}
+                    playing={isPlaying}
+                    onDuration={onDuration}
+                />
+            </div >);
     };
 }
-
 export default NewPlayer;
-
-
-
-
